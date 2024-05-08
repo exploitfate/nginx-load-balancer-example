1c1eabac93cc326b9aa35c396849167df9f5fc22
casper <casper@inernetbs.net>
2024-05-08 14:40:23 +0300

    Create example setup scripts

---
+++ lb-install.sh
@@ -0,0 +1,277 @@
+#!/bin/bash
+
+# Create domain name helper
+#NGINX_DOMAIN="domain.com"
+read -p "Domain: " NGINX_DOMAIN
+
+sudo apt update
+
+# install nginx and certbot
+sudo apt install -y nginx certbot python3-certbot-dns-cloudflare python3-certbot-nginx
+
+# Create certbot deploy renewal hook
+echo '#!/bin/bash
+
+/usr/bin/systemctl reload nginx
+' | sudo tee /etc/letsencrypt/renewal-hooks/deploy/nginx.sh > /dev/null
+
+sudo chmod +x /etc/letsencrypt/renewal-hooks/deploy/nginx.sh
+
+# Request lets encrypt cert for domain and www subdomain
+sudo certbot certonly --dry-run --nginx --email admin@$NGINX_DOMAIN -d $NGINX_DOMAIN -d www.$NGINX_DOMAIN && sudo certbot certonly --nginx --email admin@$NGINX_DOMAIN -d $NGINX_DOMAIN -d sub.$NGINX_DOMAIN
+
+## Alternate: New cert chain and cert type
+# sudo apt-get remove -y certbot
+# sudo snap install --classic certbot
+# sudo ln -s /snap/bin/certbot /usr/bin/certbot
+# certbot --version
+# certbot 2.5.0
+# sudo certbot --force-renewal --preferred-chain  "ISRG Root X1" renew  --key-type ecdsa
+
+# Verify certs
+sudo certbot certificates
+
+# Generate domain Diffie-Hellman parameter for DHE ciphersuites
+sudo mkdir -p /etc/nginx/ssldhparam/$NGINX_DOMAIN
+sudo openssl dhparam -out /etc/nginx/ssldhparam/$NGINX_DOMAIN/dhparam.pem 2048
+
+# Increase www-data user open file limits
+echo '
+www-data         soft    nofile           8192
+www-data         hard    nofile           8192
+' | sudo tee /etc/security/limits.d/www-data.conf > /dev/null
+
+sudo su www-data -s /bin/bash -c 'ulimit -Sn'
+
+# Create backup nginx.conf
+sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.default
+
+# Increase nginx open file and connections limits
+sudo sed -i '3iworker_rlimit_nofile 8192;' /etc/nginx/nginx.conf
+sudo sed -i 's/worker_connections 768/worker_connections 8192/g' /etc/nginx/nginx.conf
+
+# Create bash cron task to update cloudflare ip helper to fix and replace Cloudflare IP Address by real user IP Address
+cd /opt && sudo git clone https://github.com/exploitfate/cfip.git
+sudo chmod +x /opt/cfip/update.sh
+sudo /opt/cfip/update.sh
+
+echo '# /etc/cron.d/cfip: crontab entries to update Cloudflare IPs for nginx config
+SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+0 0 * * * /opt/cfip/update.sh
+' | sudo tee /etc/cron.d/cfip > /dev/null
+
+# Increase server names hashes
+echo '
+server_names_hash_max_size 1024;
+server_names_hash_bucket_size 128;
+' | sudo tee /etc/nginx/conf.d/names_hash.conf > /dev/null
+
+# Tune gzip
+echo '
+# Gzip Settings
+#gzip on;               # enabled by default
+#gzip_disable "msie6";  # enabled by default
+gzip_vary on;
+gzip_proxied any;
+gzip_comp_level 6;
+gzip_buffers 16 8k;
+gzip_types
+    application/atom+xml
+    application/javascript
+    application/x-javascript
+    application/json
+    application/ld+json
+    application/manifest+json
+    application/rss+xml
+    application/vnd.geo+json
+    application/vnd.ms-fontobject
+    application/x-font-ttf
+    application/x-web-app-manifest+json
+    application/xhtml+xml
+    application/xml
+    font/opentype
+    image/bmp
+    image/svg+xml
+    image/x-icon
+    text/cache-manifest
+    text/css
+    text/plain
+    text/vcard
+    text/vnd.rim.location.xloc
+    text/vtt
+    text/x-component
+    text/x-cross-domain-policy;
+
+' | sudo tee /etc/nginx/conf.d/gzip.conf > /dev/null
+
+# Set charset
+echo '
+#Specify a charset
+charset utf-8;
+
+client_max_body_size 128M;
+#server_tokens off;
+
+merge_slashes off;
+
+fastcgi_buffer_size 64k;
+fastcgi_buffers 8 64k;
+' | sudo tee /etc/nginx/conf.d/charset.conf > /dev/null
+
+## Set upstream backends
+echo '
+upstream backend {
+    server ip-172-31-40-1.us-east-2.compute.internal;
+    server ip-172-31-44-2.us-east-2.compute.internal;
+    server ip-172-31-35-3.us-east-2.compute.internal;
+    server ip-172-31-38-4.us-east-2.compute.internal;
+}
+
+' | sudo tee /etc/nginx/conf.d/api.conf > /dev/null
+
+
+
+# Turn off auth_basic for listed locations
+echo '
+location /.well-known {
+        auth_basic off;
+        try_files $uri $uri/;
+}
+' | sudo tee /etc/nginx/noauth.conf > /dev/null
+
+# Create auth_basic password file
+read -p "Username: " USERNAME
+read -s -p "Password: " PASSWORD;
+echo printf "${USERNAME}:$(openssl passwd -crypt ${PASSWORD})\n" | sudo tee /etc/nginx/passwords > /dev/null
+
+# Deny access for files with listed extensions
+echo '
+# Prevent clients from accessing hidden files (starting with a dot)
+# This is particularly important if you store .htpasswd files in the site hierarchy
+
+# Let’s Encrypt validation requests
+location ~ /.well-known {
+    allow all;
+}
+
+# Prevent clients from accessing to backup/config/source files
+location ~* (?:\.(?:bak|config|sql|fla|psd|ini|log|sh|inc|swp|dist|md)|~)$ {
+    deny all;
+}
+
+# Prevent clients from accessing to backup/config/source files
+location ~* (?:\.(?:bak|config|sql|fla|psd|ini|log|sh|inc|swp|dist|md)|~)$ {
+    deny all;
+}
+' | sudo tee /etc/nginx/protect-system-files.conf > /dev/null
+
+## Create load balancer domain config
+echo '
+# HTTP
+server {
+        server_name '$NGINX_DOMAIN' www.'$NGINX_DOMAIN';
+        listen 80;
+
+        root          /var/www/html;
+
+        # auth_basic "Protected";
+        # auth_basic_user_file passwords;
+        # include noauth.conf;
+
+        # access_log    /var/log/nginx/access.log combined buffer=64k;
+        access_log    off;
+        error_log     /var/log/nginx/error.log notice;
+
+        include protect-system-files.conf;
+
+        location / {
+            proxy_pass http://backend;
+            proxy_set_header Host $host;
+            # Set real user IP Address to X-Real-IP header
+            proxy_set_header X-Real-IP $remote_addr;
+        }
+}
+
+# HTTP to HTTPS and www to non-www redirect
+#server {
+#        server_name '$NGINX_DOMAIN' www.'$NGINX_DOMAIN';
+#        listen 80;
+#        return 301 https://'$NGINX_DOMAIN'$request_uri;
+#}
+
+# HTTPS www to non-www redirect 443
+#server {
+#        server_name www.'$NGINX_DOMAIN';
+#        listen 443 ssl http2;
+#        ssl_certificate /etc/letsencrypt/live/'$NGINX_DOMAIN'/fullchain.pem;
+#        ssl_certificate_key /etc/letsencrypt/live/'$NGINX_DOMAIN'/privkey.pem;
+#        ssl_dhparam /etc/nginx/ssldhparam/'$NGINX_DOMAIN'/dhparam.pem;
+#        # enable ocsp stapling (mechanism by which a site can convey certificate revocation information to visitors in a privacy-preserving, scalable manner)
+#        # http://blog.mozilla.org/security/2013/07/29/ocsp-stapling-in-firefox/
+#        resolver 8.8.8.8 8.8.4.4;
+#        ssl_stapling on;
+#        ssl_stapling_verify on;
+#        return 301 https://'$NGINX_DOMAIN'$request_uri;
+#}
+
+# HTTPS
+server {
+        server_name '$NGINX_DOMAIN';
+        listen 443 ssl http2;
+
+        ssl_certificate /etc/letsencrypt/live/'$NGINX_DOMAIN'/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/'$NGINX_DOMAIN'/privkey.pem;
+
+        # Diffie-Hellman parameter for DHE ciphersuites, recommended 2048 bits
+        ssl_dhparam /etc/nginx/ssldhparam/'$NGINX_DOMAIN'/dhparam.pem;
+
+        # enables server-side protection from BEAST attacks
+        # http://blog.ivanristic.com/2013/09/is-beast-still-a-threat.html
+        # ssl_prefer_server_ciphers on;
+        # disable SSLv3(enabled by default since nginx 0.8.19) since it is less secure then TLS http://en.wikipedia.org/wiki/Secure_Sockets_Layer#SSL_3.0
+        # ssl_protocols TLSv1.1 TLSv1.2;
+        # ciphers chosen for forward secrecy and compatibility
+        # https://www.nginx.com/blog/pci-dss-best-practices-with-nginx-plus/
+        # ssl_ciphers ECDHE-ECDSA-CHACHA20-POLY1305:ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:!AES256-GCM-SHA256:!AES256-GCM-SHA128:!aNULL:!MD5;
+
+        # enable ocsp stapling (mechanism by which a site can convey certificate revocation information to visitors in a privacy-preserving, scalable manner)
+        # http://blog.mozilla.org/security/2013/07/29/ocsp-stapling-in-firefox/
+        resolver 8.8.8.8 8.8.4.4;
+        ssl_stapling on;
+        ssl_stapling_verify on;
+        # config to enable HSTS(HTTP Strict Transport Security) https://developer.mozilla.org/en-US/docs/Security/HTTP_Strict_Transport_Security
+        # to avoid ssl stripping https://en.wikipedia.org/wiki/SSL_stripping#SSL_stripping
+        # also https://hstspreload.org/
+#        add_header Strict-Transport-Security "max-age=31536000";
+
+        root          /var/www/html;
+
+        # auth_basic "Protected";
+        # auth_basic_user_file passwords;
+        # include noauth.conf;
+
+        # access_log    /var/log/nginx/access.log combined buffer=64k;
+        access_log    off;
+        error_log     /var/log/nginx/error.log notice;
+
+        include protect-system-files.conf;
+
+        location / {
+            proxy_pass http://backend;
+            proxy_set_header Host $host;
+            # Set real user IP Address to X-Real-IP header
+            proxy_set_header X-Real-IP $remote_addr;
+        }
+}
+' | sudo tee /etc/nginx/sites-available/$NGINX_DOMAIN.conf > /dev/null
+
+# Enable load balancer domain config
+cd /etc/nginx/sites-enabled/
+
+sudo ln -s ../sites-available/$NGINX_DOMAIN.conf .
+
+sudo nginx -t
+sudo service nginx restart
+
---
+++ upstream-install.sh
@@ -0,0 +1,296 @@
+#!/bin/bash
+
+# Create domain name helper
+#NGINX_DOMAIN="domain.com"
+read -p "Domain: " NGINX_DOMAIN
+
+sudo apt update
+
+# install nginx and php
+sudo apt install -y nginx php-fpm php-cli
+
+# Increase www-data user open file limits
+echo '
+www-data         soft    nofile           2048
+www-data         hard    nofile           2048
+' | sudo tee /etc/security/limits.d/www-data.conf > /dev/null
+sudo su www-data -s /bin/bash -c 'ulimit -Sn'
+
+# Create backup nginx.conf
+sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.default
+
+# Increase nginx open file and connections limits
+sudo sed -i '3iworker_rlimit_nofile 2048;' /etc/nginx/nginx.conf
+sudo sed -i 's/worker_connections 768/worker_connections 2048/g' /etc/nginx/nginx.conf
+
+
+# Tune php-fpm workers children
+CPU_COUNT=$(nproc)
+MIN_SPARE_SERVERS=$CPU_COUNT
+MAX_SPARE_SERVERS=$(($CPU_COUNT*3))
+START_SERVERS=$((($MIN_SPARE_SERVERS+$MAX_SPARE_SERVERS)/2))
+MAX_CHILDREN=$(($CPU_COUNT*3*2))
+for PHP_VERSION in $(ls /etc/php/) ; do   sudo cp /etc/php/$PHP_VERSION/fpm/pool.d/www.conf /etc/php/$PHP_VERSION/fpm/pool.d/www.conf.default ;   sudo sed -i "s/^pm.max_children.*/pm.max_children = $MAX_CHILDREN/g" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf ;   sudo sed -i "s/^pm.start_servers.*/pm.start_servers = $START_SERVERS/g" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf ;   sudo sed -i "s/^pm.min_spare_servers.*/pm.min_spare_servers = $MIN_SPARE_SERVERS/g" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf ;   sudo sed -i "s/^pm.max_spare_servers.*/pm.max_spare_servers = $MAX_SPARE_SERVERS/g" /etc/php/$PHP_VERSION/fpm/pool.d/www.conf ;   sudo php-fpm$PHP_VERSION -t && sudo service php$PHP_VERSION-fpm reload; done;
+
+# php tune
+echo '
+cgi.fix_pathinfo= 0
+expose_php = Off
+' | sudo tee /etc/php/7.4/mods-available/nginx.ini && sudo phpenmod nginx
+echo '
+memory_limit = 512M
+post_max_size = 128M
+upload_max_filesize = 128M
+' | sudo tee /etc/php/7.4/mods-available/php-override.ini && sudo phpenmod php-override
+
+
+# Set charset
+echo '
+#Specify a charset
+charset utf-8;
+
+client_max_body_size 128M;
+#server_tokens off;
+
+merge_slashes off;
+
+fastcgi_buffer_size 64k;
+fastcgi_buffers 8 64k;
+' | sudo tee /etc/nginx/conf.d/charset.conf > /dev/null
+
+
+# Tune gzip
+echo '
+# Gzip Settings
+#gzip on;               # enabled by default
+#gzip_disable "msie6";  # enabled by default
+gzip_vary on;
+gzip_proxied any;
+gzip_comp_level 6;
+gzip_buffers 16 8k;
+gzip_types
+    application/atom+xml
+    application/javascript
+    application/x-javascript
+    application/json
+    application/ld+json
+    application/manifest+json
+    application/rss+xml
+    application/vnd.geo+json
+    application/vnd.ms-fontobject
+    application/x-font-ttf
+    application/x-web-app-manifest+json
+    application/xhtml+xml
+    application/xml
+    font/opentype
+    image/bmp
+    image/svg+xml
+    image/x-icon
+    text/cache-manifest
+    text/css
+    text/plain
+    text/vcard
+    text/vnd.rim.location.xloc
+    text/vtt
+    text/x-component
+    text/x-cross-domain-policy;
+
+' | sudo tee /etc/nginx/conf.d/gzip.conf > /dev/null
+
+# Increase server names hashes
+echo '
+server_names_hash_max_size 1024;
+server_names_hash_bucket_size 128;
+' | sudo tee /etc/nginx/conf.d/names_hash.conf > /dev/null
+
+# Increase server names hashes
+echo '
+server_names_hash_max_size 1024;
+server_names_hash_bucket_size 128;
+' | sudo tee /etc/nginx/conf.d/names_hash.conf > /dev/null
+
+
+# Increase server names hashes
+echo '
+# nginx load balancer IP Address
+# # Fix and replace load balancer IP Address by real user IP Address from X-Real-IP header
+
+# Load balancer private IP Address
+set_real_ip_from 172.31.35.1;
+
+real_ip_header X-Real-IP;
+' | sudo tee /etc/nginx/conf.d/lb.conf > /dev/null
+
+# Create auth_basic password file
+read -p "Username: " USERNAME
+read -s -p "Password: " PASSWORD;
+echo printf "${USERNAME}:$(openssl passwd -crypt ${PASSWORD})\n" | sudo tee /etc/nginx/passwords > /dev/null
+
+
+# Turn off auth_basic for listed locations
+echo '
+location /.well-known {
+        auth_basic off;
+        try_files $uri $uri/;
+}
+' | sudo tee /etc/nginx/noauth.conf > /dev/null
+
+# Deny access for files with listed extensions
+echo '
+# Prevent clients from accessing hidden files (starting with a dot)
+# This is particularly important if you store .htpasswd files in the site hierarchy
+
+# Let’s Encrypt validation requests
+location ~ /.well-known {
+    allow all;
+}
+
+# Prevent clients from accessing to backup/config/source files
+location ~* (?:\.(?:bak|config|sql|fla|psd|ini|log|sh|inc|swp|dist|md)|~)$ {
+    deny all;
+}
+
+# Prevent clients from accessing to backup/config/source files
+location ~* (?:\.(?:bak|config|sql|fla|psd|ini|log|sh|inc|swp|dist|md)|~)$ {
+    deny all;
+}
+' | sudo tee /etc/nginx/protect-system-files.conf > /dev/null
+
+# Tune webfont cache
+echo '
+# Cross domain webfont access
+location ~* \.(?:ttf|ttc|otf|eot|woff|woff2)$ {
+    # Also, set cache rules for webfonts.
+    #
+    # See http://wiki.nginx.org/HttpCoreModule#location
+    # And https://github.com/h5bp/server-configs/issues/85
+    # And https://github.com/h5bp/server-configs/issues/86
+    expires 1M;
+    access_log off;
+    add_header Cache-Control "public";
+}
+' | sudo tee /etc/nginx/cross-domain-fonts.conf > /dev/null
+
+# Tune static content cache
+echo '
+# Expire rules for static content
+
+# No default expire rule. This config mirrors that of apache as outlined in the
+# html5-boilerplate .htaccess file. However, nginx applies rules by location,
+# the apache rules are defined by type. A consequence of this difference is that
+# if you use no file extension in the url and serve html, with apache you get an
+# expire time of 0s, with nginx you would get an expire header of one month in the
+# future (if the default expire rule is 1 month). Therefore, do not use a
+# default expire rule with nginx unless your site is completely static
+
+# cache.appcache, your document html and data
+location ~* \.(?:manifest|appcache|html?|xml|json)$ {
+  expires -1;
+  #access_log logs/static.log;
+}
+
+# Feed
+location ~* \.(?:rss|atom)$ {
+  expires 1h;
+  add_header Cache-Control "public";
+}
+
+# Media: images, icons, video, audio, HTC
+location ~* \.(?:jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc|webp|mp3)$ {
+  expires 1M;
+  access_log off;
+  add_header Cache-Control "public";
+}
+
+# CSS and Javascript
+location ~* \.(?:css|js)$ {
+  expires 1y;
+  access_log off;
+  add_header Cache-Control "public";
+}
+# favicon disable logs
+location = /favicon.ico {
+  access_log off;
+  log_not_found off;
+}
+' | sudo tee /etc/nginx/expires.conf > /dev/null
+
+# Create php-fpm handler
+echo '
+set           $bootstrap  index.php;
+index         $bootstrap;
+
+location / {
+        proxy_set_header Host $host;
+        add_header Access-Control-Allow-Methods "GET, POST, PUT, DELETE, OPTIONS";
+        add_header Access-Control-Allow-Origin $http_origin;
+        add_header Access-Control-Allow-Headers "Authorization, Content-Type";
+        add_header Access-Control-Allow-Credentials true;
+        if ($request_method = OPTIONS) {
+                add_header Content-Length 0;
+                add_header Content-Type text/plain;
+                add_header Access-Control-Allow-Methods "GET, POST, PUT, DELETE, OPTIONS";
+                add_header Access-Control-Allow-Origin $http_origin;
+                add_header Access-Control-Allow-Headers "Authorization, Content-Type";
+                add_header Access-Control-Allow-Credentials true;
+                return 200;
+        }
+        try_files $uri $uri/ /$bootstrap$is_args$args;
+}
+location ~ \.php$ {
+        try_files $uri =404;
+
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_index               $bootstrap;
+
+        fastcgi_pass unix:/var/run/php/php-fpm.sock;
+
+        fastcgi_connect_timeout     30s;
+        fastcgi_read_timeout        30s;
+        fastcgi_send_timeout        60s;
+        fastcgi_ignore_client_abort on;
+        fastcgi_pass_header         "X-Accel-Expires";
+
+        fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
+        fastcgi_param  PATH_INFO        $fastcgi_path_info;
+        fastcgi_param  HTTP_REFERER     $http_referer;
+        include fastcgi_params;
+}
+' | sudo tee /etc/nginx/php-fpm-bootstrap.conf
+
+# Create upstream php backend config
+echo '
+# upstream php backend
+server {
+        server_name default_server;
+
+        listen 80;
+
+        root          /var/www/html;
+
+        # auth_basic "Protected";
+        # auth_basic_user_file passwords;
+        # include noauth.conf;
+
+        access_log    /var/log/nginx/access.log combined buffer=1024k;
+        error_log     /var/log/nginx/error.log notice;
+
+        include cross-domain-fonts.conf;
+        include protect-system-files.conf;
+        include expires.conf;
+
+        include php-fpm-bootstrap.conf;
+
+}
+' | sudo tee /etc/nginx/sites-available/$NGINX_DOMAIN.conf > /dev/null
+
+# Enable upstream php backend config
+cd /etc/nginx/sites-enabled/
+
+sudo ln -s ../sites-available/$NGINX_DOMAIN.conf .
+
+# Remove default nginx config
+sudo rm /etc/nginx/sites-enabled/default
+
+sudo nginx -t
+sudo service nginx restart
